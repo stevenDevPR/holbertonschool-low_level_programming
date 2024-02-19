887b37f7f81f8bfbcaca0aa8db97ec2de05c1560
stevendevpr <stevenbonilla46@yahoo.com>
2024-02-19 12:13:31 -0400

    $comment

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - start the program
+ *
+ * return: 0
+ * 
+ * */
+
 int main(void)
 {
 
-	printf("with proper grammar, but the outcome is a piece of art\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 
