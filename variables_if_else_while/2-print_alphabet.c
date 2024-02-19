6220145ff44a6132dc0be7b1a6f1ffe5d8085a6a
stevendevpr <stevenbonilla46@yahoo.com>
2024-02-19 18:54:23 -0400

    task 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - start of program
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	for (char letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+		putchar('\n');
+
+
+	}
+
+
+	return (0);
+}
+
