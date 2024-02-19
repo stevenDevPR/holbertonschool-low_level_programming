fa3a01fd491809c292a9828e0c1c5e5a225e9fee
stevendevpr <stevenbonilla46@yahoo.com>
2024-02-19 11:27:40 -0400

    $comment

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * int main(void)= main function
+ * const char *message= message as a constant with character type
+ * return (0) = when finish correctly without error will return 0, if error is present it will return 1 */
+
 int main(void)
 {
 	const char *message = "\"Programming is like building a multilingual puzzle";
