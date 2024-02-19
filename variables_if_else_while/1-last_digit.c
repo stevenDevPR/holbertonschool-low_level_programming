30050ae0b680fb61c56c77f7c20dfbd581209f0c
stevendevpr <stevenbonilla46@yahoo.com>
2024-02-19 18:18:22 -0400

    task 1 final

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -10,10 +10,15 @@
 
 int main(void)
 {
-	int n = rand() - RAND_MAX / 2;
-	int lastDigit = n % 10;
+	int n;
 
 	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	int lastDigit;
+
+	lastDigit = n % 10;
+
 	/**n = rand() - RAND_MAX / 2;*/
 
 	if (n > 5)
