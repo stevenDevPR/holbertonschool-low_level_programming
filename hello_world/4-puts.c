d993bb5817990b62d12aac2bbb3df1cc6f473950
stevendevpr <stevenbonilla46@yahoo.com>
2024-02-19 11:54:31 -0400

    $comment

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -6,7 +6,7 @@
 
 int main(void)
 {
-	char  message[]= "\"Programming is like building a multilingual puzzle";
+	char  message[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(message);
 
