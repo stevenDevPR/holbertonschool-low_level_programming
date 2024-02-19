940c6761aeaa03eb4c026c76207665e7ad687ad2
stevendevpr <stevenbonilla46@yahoo.com>
2024-02-19 19:45:02 -0400

    task 5 completed

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - starting program
+ *
+ * Return: 0
+ */
+
+
+int main(void)
+{
+	int numbers;
+
+	for (numbers = 0; numbers <= 9; numbers++)
+	{
+		printf("%d", numbers);
+	}
+
+	return (0);
+}
