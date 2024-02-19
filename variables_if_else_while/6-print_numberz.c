2f72bc4242b8ea72829b8cd0cc5b9242ac18c26d
stevendevpr <stevenbonilla46@yahoo.com>
2024-02-19 19:56:02 -0400

    task 6 completed

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - start of program
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int numbers;
+
+	for (numbers = 0; numbers <= 9; numbers++)
+	{
+		putchar('0' + numbers);
+	}
+	putchar('\n');
+
+
+
+
+	return (0);
+}
