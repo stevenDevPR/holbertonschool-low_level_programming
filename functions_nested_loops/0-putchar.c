c9fe734debf3996fd7c59fe3cbba57e49c03df95
stevendevpr <stevenbonilla46@yahoo.com>
2024-02-19 22:37:04 -0400

    task 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -16,8 +16,14 @@ int main(void)
 void print_putchar(void)
 {
 	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
 	_putchar('a');
-	_putchar('a');
-	_putchar('a');
-	_putchar('a');
+	_putchar('r');
+}
+}
+}
 }
