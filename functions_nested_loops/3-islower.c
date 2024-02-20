0e5a6597ff9b70992ff39dccd4a74bea0e0d2859
stevendevpr <stevenbonilla46@yahoo.com>
2024-02-20 16:51:46 -0400

    task 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,32 @@
+#include "main"
+/**
+ * islower - function main
+ *
+ * Return: 0
+ */
+
+int _islower(int c);
+
+int islower(void)
+{
+	is_lower();
+
+	return (0);
+
+}
+
+/**
+ * _islower - finding if c is lower
+ */
+
+int _islower(int c)
+{
+	int c;
+
+	if (c == 99)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
