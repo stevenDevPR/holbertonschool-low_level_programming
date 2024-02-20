37ee3b80d4cf70dcf1cb0449be3ecb0124b580af
stevendevpr <stevenbonilla46@yahoo.com>
2024-02-20 17:44:10 -0400

    task 4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * character - function to start program
+ *
+ * Return: 0
+ */
+
+int _isalpha(int c);
+
+int character(void)
+{
+	_isalpha('a');
+
+	return (0);
+
+}
+
+/**
+ * _isalpha - function to check if is a letter
+ *
+ * @c: value to be checked
+ *
+ * Return: 1 if c is a letter, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
