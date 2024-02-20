a855bdccfd56900c414227afe9b68095aecada1d
stevendevpr <stevenbonilla46@yahoo.com>
2024-02-19 20:01:25 -0400

    task 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - start of program
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alphabetReverse;
+
+	for (alphabetReverse = 'z'; alphabetReverse >= 'a'; alphabetReverse--)
+	{
+		putchar(alphabetReverse);
+	}
+	putchar('\n');
+
+
+
+
+	return (0);
+}
