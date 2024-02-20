95cb8a428d738c15232f4eec2bc61baf5e2c9531
stevendevpr <stevenbonilla46@yahoo.com>
2024-02-20 18:02:33 -0400

    task 5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -26,15 +26,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('43');
+		_putchar(43);
 		return (1);
 	}
 	else if (n < 0)
 	{
-		_putchar('45');
+		_putchar(45);
 		return (-1);
 	}
 	else
-		_putchar('0');
+	{
+		_putchar(0);
 		return (0);
+	}
 }
