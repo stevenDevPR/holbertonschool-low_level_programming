93dbd90ab1a2be4706a1f372c6de419389b5402b
stevendevpr <stevenbonilla46@yahoo.com>
2024-02-20 19:36:28 -0400

    task 6 completed

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,4 +1,9 @@
 #include "main.h"
+/**
+ * absolute - main function
+ *
+ * Return: 0
+ */
 
 int absolute(void)
 {
@@ -7,15 +12,24 @@ int absolute(void)
 	return (0);
 }
 
+/**
+ * _abs - function to calculate absolute value
+ *
+ * @num: value to check
+ *
+ * Return: -num if negative, num if is positive
+ */
+
 
 int _abs(int num)
 {
 
 	if (num < 0)
 	{
-		return -num;
+		return (-num);
 	}
-	else {
-		return num;
+	else
+	{
+		return (num);
 	}
 }
