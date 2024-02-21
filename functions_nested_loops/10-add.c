af771bf87c2588a5434faadb81f95541eec88118
stevendevpr <stevenbonilla46@yahoo.com>
2024-02-21 11:05:39 -0400

    task 10 completeed

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,5 +1,10 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * integer - main function
+ *
+ * Return: Always 0;
+ */
 
 int integer(void)
 {
@@ -8,6 +13,16 @@ int integer(void)
 	return (0);
 
 }
+
+/**
+ * add - function to add two integers
+ *
+ * @num1: value to sum
+ *
+ * @num2: value to sum
+ *
+ * Return: will return the sum
+ */
 
 int add(int num1, int num2)
 {
