f4cbfffd77997c4948f6d7d194befe0312561d84
stevendevpr <stevenbonilla46@yahoo.com>
2024-02-20 22:01:03 -0400

    task 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,4 +1,9 @@
 #include "main.h"
+/**
+ * minutes - function :x
+ *
+ * Return: 0
+ */
 
 int minutes(void)
 {
@@ -7,19 +12,24 @@ int minutes(void)
 	return (0);
 
 }
+/**
+ * jack_bauer -function to make all hours and minutes
+ */
 
 void jack_bauer(void)
 {
 	int i;
 	int j;
 
-	for (i = 0; i <= 23; i++)
+		for (i = 0; i <= 23; i++)
 		{
 			for (j = 0; j <= 59; j++)
 			{
-				_putchar(i + 0);
+				_putchar('0' + i / 10);
+				_putchar('0' + i % 10);
 				_putchar(':');
-				_putchar(j + 0);
+				_putchar('0' + j / 10);
+				_putchar('0' + j % 10);
 				_putchar('\n');
 			}
 		}
