76b590daeab5f35afa5b987ab3dce912e9b9a78e
stevendevpr <stevenbonilla46@yahoo.com>
2024-02-21 10:24:26 -0400

    task 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -16,9 +16,9 @@ void times_table(void)
     int i, j;
     int result;
 
-    for (i = 0; i < 9; i++)
+    for (i = 0; i <= 9; i++)
     {
-        for (j = 0; j < 9; j++)
+        for (j = 0; j <= 9; j++)
         {
             result = i * j;
 
