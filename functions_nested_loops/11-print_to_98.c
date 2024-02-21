4be93894ada0982fd180fa19f28b87d5bb7873a3
stevendevpr <stevenbonilla46@yahoo.com>
2024-02-21 14:11:25 -0400

    task 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -11,13 +11,14 @@ int natural(void)
 
 void print_to_98(int n)
 {
-
-
-	for (n = n; n < 98; n++)
+	if (n < 98)
 	{
+		for (n = n; n <= 98; n++)
+		{
+	
 		printf("%d ,", n);
-
-		if (n == 98)
+		}
+		else if (n == 98)
 		{
 			printf("%d", n);
 		}
