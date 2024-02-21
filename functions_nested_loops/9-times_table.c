52881eecfed33d4d04bceae01f86478f341f9f55
stevendevpr <stevenbonilla46@yahoo.com>
2024-02-21 10:31:17 -0400

    task 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -28,7 +28,7 @@ void times_table(void)
                 _putchar(32);
             }
             if (result < 10)
-              
+              _putchar(32);
             else
 	    {
                 _putchar(result / 10 + '0');
