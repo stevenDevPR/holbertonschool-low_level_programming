852fee68ad31300ef8c9cf3438bf6456e34d1417
stevendevpr <stevenbonilla46@yahoo.com>
2024-02-22 12:05:00 -0400

    task 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -25,7 +25,7 @@ void print_to_98(int n)
 				}
 				else
 				{
-					printf(" %d", i);
+					printf("%d", i);
 				}
 			}
 	}
@@ -39,7 +39,7 @@ void print_to_98(int n)
 				}
 				else
 				{
-					printf(" %d", i);
+					printf("%d", i);
 				}
 			}
 		}
