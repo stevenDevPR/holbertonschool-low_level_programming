dc1b5cf49252df6372f3f7fc85de3653cb5baf2d
stevendevpr <stevenbonilla46@yahoo.com>
2024-02-22 11:50:16 -0400

    task 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -13,25 +13,29 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n ; i <= 98; i++)
+	i = n;
+
+	if (i <= 98)
+	{	
+		for (i = n; i <= 98; i++;)
+			{
+				if (i < 98)
+			{	
+				printf("%d,", i);
+			}
+			else
+			{
+				printf("%d", i);
+			}
+			}
+	}
+	if ( i > 98)
 	{
-		if (i < 98)
-		{
-			printf("%d,", i);
-		}
-		else
-		{
-			printf("%d", i);
-		}
-		if ( i > 98)
-		{
-			for (i = n; i >= 98; i--)
+		for (i = n; i >= 98; i--)
 			{
 				printf("%d, ", i);
 			}
 		}
-	}
-
 	printf("\n");
 }
 
