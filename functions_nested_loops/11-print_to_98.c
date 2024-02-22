00b6178c48db974aa3e6376217aa83fa9044d3aa
stevendevpr <stevenbonilla46@yahoo.com>
2024-02-22 12:13:08 -0400

    task 11 FINISHED

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include "main.h"
+/**
+ * natural - main function to program
+ *
+ * Return: Always 0
+ */
 
 int natural(void)
 {
@@ -8,6 +13,13 @@ int natural(void)
 	return (0);
 
 }
+/**
+ * print_to_98 - main function to print until 98
+ *
+ * @n: value to be checked
+ *
+ *Return: Always 0
+ */
 
 void print_to_98(int n)
 {
@@ -16,38 +28,40 @@ void print_to_98(int n)
 	i = n;
 
 	if (i < 98)
-	{	
-		for (i = n; i <= 98; i++)
-			{
-				if (i < 98)
-				{	
-					printf("%d, ", i);
-				}
-				else
-				{
-					printf("%d", i);
-				}
-			}
-	}
-	if ( i > 98)
 	{
-		for (i = n; i >= 98; i--)
+		for (i = n; i <= 98; i++)
+		{
+			if (i < 98)
 			{
-				if ( i > 98)
-				{
 				printf("%d, ", i);
-				}
-				else
-				{
-					printf("%d", i);
-				}
+			}
+			else
+			{
+				printf("%d", i);
 			}
 		}
-	else 
+	}
+	if (i > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i > 98)
+			{
+				printf("%d, ", i);
+			}
+			else
+			{
+				printf("%d", i);
+			}
+		}
+	}
+	else
 	{
 		printf("%d", i);
 	}
 	printf("\n");
+
+	return (0);
 }
 
 
