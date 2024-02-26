6a8572f76b48137d93f5dad43126857789b54ee3
stevendevpr <stevenbonilla46@yahoo.com>
2024-02-26 14:37:51 -0400

    task 7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -12,9 +12,9 @@ void print_diagonal(int n)
 
 	if (n > 0)
 	{
-		for (i = 0; i <= n; i++)
+		for (i = 1; i <= n; i++)
 		{
-			for (j = 0; j <= i; j++)
+			for (j = 0; j < i; j++)
 			{
 				if (j != 0)
 				{
