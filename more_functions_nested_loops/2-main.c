e1525f8907576c5a7a3359949adf719eafc4a9b4
stevendevpr <stevenbonilla46@yahoo.com>
2024-02-26 09:45:21 -0400

    task 2

---
+++ more_functions_nested_loops/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - main function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	mul(5,6);
+
+	return (0);
+}
---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * mul - function that multiplies two integers
+ *
+ * @a: value to be checked
+ *
+ * @b: value to be checked
+ *
+ * Return: mult
+ */
+
+int mul(int a, int b)
+{
+	int mult;
+
+	mult = a * b;
+
+	return (mult);
+}
