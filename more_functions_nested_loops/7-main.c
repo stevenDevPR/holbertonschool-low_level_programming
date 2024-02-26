0cbfec69a7ee0d3cd64fc5cc0445b397d2c2fe37
stevendevpr <stevenbonilla46@yahoo.com>
2024-02-26 12:57:34 -0400

    task 7

---
+++ more_functions_nested_loops/7-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - function to print
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	print_diagonal(15);
+
+	return (0);
+
+}
---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_diagonal - print diagonal design
+ *
+ * @n: value to use
+ */
+
+void print_diagonal(int n)
+{
+	char slash = '\';
+
+	char less = '\n';
+
+	int i;
+
+	for (i = 1; i <= n; i++)
+	{
+		_putchar(slash);
+
+		if (n <= 0)
+		{
+			_putchar(less);
+		}
+	}
+	_putchar('\n');
+}
