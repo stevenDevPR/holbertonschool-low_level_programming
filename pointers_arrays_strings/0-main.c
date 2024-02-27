fd28f5ba8292ae01b40328e5c1fca3b596249563
stevendevpr <stevenbonilla46@yahoo.com>
2024-02-26 22:49:39 -0400

    task 0

---
+++ pointers_arrays_strings/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - function of program
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num = 42;
+
+	reset_to_98(&num);
+
+	return (0);
+
+}
---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * reset_to_98 - main function
+ *
+ * @*n: pointer
+ */
+
+void reset_to_98(int *n)
+{
+	if (n != NULL)
+	{
+		*n = 98;
+	}
+}
