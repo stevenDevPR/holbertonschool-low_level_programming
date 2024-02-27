1d11c03e86ef40a36db6fc9992bde130358da4ec
stevendevpr <stevenbonilla46@yahoo.com>
2024-02-27 17:37:49 -0400

    task 5 done

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -3,26 +3,23 @@
 #include <stdlib.h>
 #include <unistd.h>
 /**
- * print_rev - function that return the length of a string
+ * rev_string - function that return the length of a string
  *
  * @s: pointer
- *
- * Return: will return the value of i
  */
 
 void rev_string(char *s)
 {
 
-		int len, reverse, i = 0;
+		int reverse, i = 0;
 
 		while (s[i] != '\0')
 		{
 			i++;
 		}
 
-		len = i;
 
-		for (reverse = len - 1; reverse >= 0; reverse--)
+		for (reverse = i - 1; reverse >= 0; reverse--)
 		{
 			printf("%c", s[reverse]);
 		}
