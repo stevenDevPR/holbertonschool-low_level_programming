f39fc00480a5e2906f7c87c8bcec11c0a6eafc7f
stevendevpr <stevenbonilla46@yahoo.com>
2024-02-26 20:18:16 -0400

    task 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -21,20 +21,19 @@ void multiples(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i <= 100; ++i)
 	{
-
-		if (i % 5 == 0)
+		if ((i % 5 == 0) && (i % 3 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if	(i % 5 == 0)
 		{
 			printf("Buzz ");
 		}
 		else if (i % 3 == 0)
 		{
 			printf("Fizz ");
-		}
-		else if ((i % 5 == 0) && (i % 3 == 0))
-		{
-			printf("FizzBuzz ");
 		}
 		else
 		{
