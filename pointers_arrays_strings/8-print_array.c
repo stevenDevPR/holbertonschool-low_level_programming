c74495e9db75b708e75c0bf7cf4ae86a93cdb594
stevendevpr <stevenbonilla46@yahoo.com>
2024-02-27 23:30:56 -0400

    task 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - main function
+ *
+ * @a: pointer
+ *
+ * @n: number of elements to be printed
+ */
+
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <= n - 1; i++)
+	{
+		if (i ==  n - 1)
+		{
+			printf("%d", a[i]);
+		}
+		else
+		printf("%d, ", a[i]);
+	}
+	printf("\n");
+}
