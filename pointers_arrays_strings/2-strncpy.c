a89bef66994be6e36a0a24c5efa749578bd8d340
stevendevpr <stevenbonilla46@yahoo.com>
2024-03-02 11:11:20 -0400

    task 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,37 +1,33 @@
 #include "main.h"
 #include <stddef.h>
-/**
- * _strncpy - main function
- *
- * @dest: pointer to destination
- *
- * @src: pointer of the source
- *
- * @n: number of bytes to print
- *
- * Return: dest
- */
 
+/**
+ * _strncpy - Copies up to n characters from src to dest.
+ *
+ * @dest: Pointer to the destination string.
+ * @src: Pointer to the source string.
+ * @n: Maximum number of characters to copy.
+ *
+ * Return: Pointer to the destination string.
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
-	char *origi_dest = dest;
+    size_t i = 0;
+    char *orig_dest = dest;
 
-	if (*src == '\0')
-	{
-		while (i < n)
-		{
-			*dest++ = '\0';
-			i++;
-		}
-	}
-	else
-	{
-		while ((size_t)i < (size_t)n && *src != '\0')
-		{
-		*dest++ = *src++;
-		i++;
-		}
-	}
-	return (origi_dest);
+    while (i < (size_t)n && *src != '\0')
+    {
+        *dest++ = *src++;
+        i++;
+    }
+
+    while (i < (size_t)n)
+    {
+        *dest++ = '\0';
+        i++;
+    }
+
+    *dest = '\0';
+
+    return orig_dest;
 }
