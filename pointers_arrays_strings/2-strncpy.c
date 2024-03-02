28cb01a1632f762ab5ddb66014ace184613bf50c
stevendevpr <stevenbonilla46@yahoo.com>
2024-03-02 10:10:58 -0400

    task 2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strncat - main function
+ *
+ * @dest: pointer to destination
+ *
+ * @src: pointer of the source
+ *
+ * @n: number of bytes to print
+ *
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+        size_t i = 0;
+	char *origi_dest = dest;
+
+        while ((size_t)i <= (size_t) n - 1 && (char) *src != '\0')
+        {
+                *dest++ = *src++;
+                i++;
+	}
+
+	if (*src == '\0')
+	{
+		*dest = '\0';
+	}
+        return (origi_dest);
+}
