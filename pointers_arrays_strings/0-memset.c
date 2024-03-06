6bc031ec9bc3963477d143b2f5133868d52727c5
stevendevpr <stevenbonilla46@yahoo.com>
2024-03-06 11:10:24 -0400

    task 0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -10,14 +10,14 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-    char *ptr = s;
-
-    while (n > 0)
-    {
-        *ptr = b;
-        ptr++;
-        n--;
-    }
-
-    return s;
+	char *ptr = s;
+	
+	while (n > 0)
+	{
+		*ptr = b;
+		ptr++;
+		n--;
+	}
+	
+	return (s);
 }
