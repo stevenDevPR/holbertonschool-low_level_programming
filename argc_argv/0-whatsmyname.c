477479f1837e31a94d74d9b824107a47d1ac333b
stevendevpr <stevenbonilla46@yahoo.com>
2024-03-09 12:37:16 -0400

    task 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - funciton
+ *
+ * @argc: value
+ *
+ * @argv: value
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+
+	return (0);
+}
