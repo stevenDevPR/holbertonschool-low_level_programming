f17fb443a8fb0fba91cff98ec881f011e6db2375
stevendevpr <stevenbonilla46@yahoo.com>
2024-03-09 12:57:25 -0400

    task 2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - function
+ *
+ * @argc: arguments
+ *
+ * @argv: name of arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
