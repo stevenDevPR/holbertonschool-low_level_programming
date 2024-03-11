530df539b39b03ee5fa69eb1572e0d9c9e2a1123
Steven Bonilla <steven@iMac-4.local>
2024-03-11 15:04:34 -0400

    task 0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,9 +1,39 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+/**
+*create_array - main function
+*
+*@size: size of arrray
+*
+*@c: value of array
+*
+*Return: NULL
+*/
 
 char *create_array(unsigned int size, char c)
 {
-return (0);
-}
+	unsigned int i;
+	char *array;
 
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	array = (char *)malloc(size * sizeof(char));
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+
+	return (array);
+
+}
 
