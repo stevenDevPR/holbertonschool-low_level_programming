ff2454c8a04d85e99f331048044c6441b3ff4c67
stevendevpr <stevenbonilla46@yahoo.com>
2024-03-14 10:57:21 -0400

    task 2

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * str_concat - main function
+ *
+ * @s1: value
+ *
+ * @s2: value pointer
+ *
+ * Return: result
+ */
+
+char *str_concat(const char *s1, const char *s2)
+{
+	size_t len_s1 = 0, len_s2 = 0;
+	const char *ptr_s1 = s1, *ptr_s2 = s2;
+	char *ptr_result;
+	char *result;
+
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
+
+	while (*(ptr_s1++))
+		len_s1++;
+	while (*(ptr_s2++))
+		len_s2++;
+
+	result = malloc((len_s1 + len_s2 + 1) * sizeof(char));
+
+	if (!result)
+		return (NULL);
+
+	ptr_result = result;
+
+	while (*s1)
+		*(ptr_result++) = *(s1++);
+
+	while (*s2)
+		*(ptr_result++) = *(s2++);
+
+	*ptr_result = '\0';
+
+	return (result);
+}
+
