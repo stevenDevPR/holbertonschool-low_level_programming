8c5875c8f05caeca2d386cedee958febaabac99a
stevendevpr <stevenbonilla46@yahoo.com>
2024-03-18 08:48:01 -0400

    task 2

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -4,26 +4,24 @@
  * @array: array
  * @size: size of array
  * @cmp: pointer to a function
- * @int: integer that return
  * Return: i or -1
  */
-
-
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	/**
-	 * checking that array, cmp and size are
-	 * not null
-	 */
-	if (array && cmp && size)
-		while (i < size)
-		{
-			if (cmp(array[i])
-					return (i);
-					i++;
-					}
-					return (-1);
-					}
+	if (array == NULL)
+		return (-1);
+	if (size <= 0)
+		return (-1);
+	if (cmp == NULL)
+		return (-1);
+
+	for (; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
 
