5da28850772aef9907ebb6036e4b9ed012fb4137
stevendevpr <stevenbonilla46@yahoo.com>
2024-03-23 11:01:17 -0400

    task 1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -9,11 +9,11 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	unsigned int i;
+
 	va_list args;
 
 	va_start(args, n);
-
-	unsigned int i;
 
 	for (i = 0; i < n; ++i)
 	{
