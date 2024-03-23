c1072b96ee4b38e3f021848c2904302f321c1e4b
stevendevpr <stevenbonilla46@yahoo.com>
2024-03-23 10:54:17 -0400

    task 0

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+int sum_them_all(const unsigned int n, ...)
+{
+	if (n == 0)
+		return (0);
+
+	unsigned int i, sum = 0;
+	va_list args;
+	va_start(args, n);
+
+	for (i = 0; i < n; ++i)
+	{
+		sum += va_arg(args, int);
+	}
+	
+	va_end(args);
+
+	return (sum);
+}
---
+++ variadic_functions/variadict_functions.h
@@ -0,0 +1,6 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+int sum_them_all(unsigned int n, ...);
+int _putchar(char c);
+#endif
