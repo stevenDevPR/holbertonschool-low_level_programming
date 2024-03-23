ad7ccd90dd02355dd15cdc330028f94b2e16f65a
stevendevpr <stevenbonilla46@yahoo.com>
2024-03-23 10:57:59 -0400

    task 0 completed

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,11 +1,17 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
+/**
+ * sum_them_all - main function to sum
+ * @n: value to sum
+ * Return: 0 or sum
+ */
 
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i, sum = 0;
 
 	va_list args;
+
 	va_start(args, n);
 
 	if (n == 0)
