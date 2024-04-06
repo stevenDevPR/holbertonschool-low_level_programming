9f1ed6cc19efc70b59740a36391654bdf92f8b1b
stevendevpr <stevenbonilla46@yahoo.com>
2024-04-06 19:10:50 -0400

    task 1

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_binary - Prints the binary representation of a number.
+ * @n: The number to print in binary.
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask = 1UL << 63;
+	int flag = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	while (mask > 0)
+	{
+		if ((n & mask) != 0)
+		{
+			_putchar('1');
+			flag = 1;
+		}
+		else if (flag)
+			_putchar('0');
+
+		mask >>= 1;
+	}
+}
