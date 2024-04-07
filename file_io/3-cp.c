9bc748b55eef7e37e6871cad33033f9e8920fda5
stevendevpr <stevenbonilla46@yahoo.com>
2024-04-06 21:23:40 -0400

    task last done

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -22,8 +22,8 @@ int main(int argc, char *argv[])
 
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read
-				from file %s\n", argv[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+				argv[1]), exit(98);
 
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
