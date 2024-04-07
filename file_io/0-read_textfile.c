8cec4845b2298ef2d8e449dfd96222c58a247b7c
stevendevpr <stevenbonilla46@yahoo.com>
2024-04-06 20:58:20 -0400

    task 0 done complete

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -3,7 +3,8 @@
 #include <unistd.h>
 #include <fcntl.h>
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * read_textfile - Reads a text file and prints
+ * it to the POSIX standard output.
  * @filename: The name of the file to read.
  * @letters: The number of letters it should read and print.
  *
