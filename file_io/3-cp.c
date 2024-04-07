24ab9c3c9c8eb71869a6d76ee97f08af0b21c581
stevendevpr <stevenbonilla46@yahoo.com>
2024-04-06 21:14:49 -0400

    last task

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -3,14 +3,11 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <errno.h>
-
 #define BUFFER_SIZE 1024
-
 /**
  * main - Entry point
  * @argc: Argument count
  * @argv: Argument vector
- *
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
@@ -23,14 +20,12 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
@@ -38,7 +33,6 @@ int main(int argc, char *argv[])
 		close(fd_from);
 		exit(99);
 	}
-
 	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
@@ -50,7 +44,6 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 	}
-
 	if (bytes_read == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
@@ -58,18 +51,15 @@ int main(int argc, char *argv[])
 		close(fd_to);
 		exit(98);
 	}
-
 	if (close(fd_from) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
 	}
-
 	if (close(fd_to) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
-
 	return (0);
 }
